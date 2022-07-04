56d800bbc080a5977b1041f6358d33b92756fe4f
Alex Danilenko <alexander.danilenko@revolut.com>
2022-07-04 18:44:19 +0300

    experimental test

--- server/src/testIntegration/groovy/spotty/server/SpottySpec.groovy
+++ server/src/testIntegration/groovy/spotty/server/SpottySpec.groovy
@@ -4,33 +4,49 @@ import spock.lang.Specification
 
 class SpottySpec extends Specification {
 
-    private Spotty server
+//    private Spotty server
 
     def setup() {
-        server = new Spotty(3333)
+//        server = new Spotty(3333)
     }
 
     def cleanup() {
-        server.awaitUntilStart()
-        server.close()
-        server.awaitUntilStop()
+//        server.awaitUntilStart()
+//        server.close()
+//        server.awaitUntilStop()
     }
 
     def "should not wait until start"() {
+        given:
+        var server = new Spotty()
+
         when:
         server.start()
 
         then:
         !server.isStarted()
+
+        cleanup:
+        server.awaitUntilStart()
+        server.close()
+        server.awaitUntilStop()
     }
 
     def "should wait until started"() {
+        given:
+        var server = new Spotty()
+
         when:
         server.start()
         server.awaitUntilStart()
 
         then:
         server.isStarted()
+
+        cleanup:
+        server.awaitUntilStart()
+        server.close()
+        server.awaitUntilStop()
     }
 
 }
