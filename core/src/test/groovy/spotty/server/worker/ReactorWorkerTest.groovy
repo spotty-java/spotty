791c9a44f86239f7fe78577721518eb944460ef7
Alex Danilenko <alex.danilenko.dev@gmail.com>
2022-09-16 11:52:22 +0300

    reactor improvements (#48)
    Co-authored-by: Alex Danilenko <alexander.danilenko@revolut.com>

--- core/src/test/groovy/spotty/server/worker/ReactorWorkerTest.groovy
+++ core/src/test/groovy/spotty/server/worker/ReactorWorkerTest.groovy
@@ -2,11 +2,21 @@ package spotty.server.worker
 
 import spock.lang.Specification
 
+import static java.util.concurrent.TimeUnit.SECONDS
 import static org.awaitility.Awaitility.await
 
 class ReactorWorkerTest extends Specification {
 
-    private final ReactorWorker reactorWorker = new ReactorWorker(1)
+    private final ReactorWorker reactorWorker = new ReactorWorker(
+        1,
+        10,
+        300,
+        SECONDS
+    )
+
+    def cleanup() {
+        reactorWorker.close()
+    }
 
     def "should execute action"() {
         given:
@@ -15,10 +25,19 @@ class ReactorWorkerTest extends Specification {
         when:
         reactorWorker.addTask { runIsTrue = true }
         await().until { runIsTrue }
-        reactorWorker.close()
 
         then:
         runIsTrue
+    }
+
+    def "should increase workers"() {
+        when:
+        for (i in 0..<1000) {
+            reactorWorker.addTask { Thread.sleep(10) }
+        }
+
+        then:
+        reactorWorker.reactorPool().poolSize == 10
     }
 
 }
